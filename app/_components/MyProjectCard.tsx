e4258a88ca033309d3e2a2f763c0cd352df44cf6
nojiritakeshi <takesoccer08nine@ezweb.ne.jp>
2024-07-30 01:28:40 +0900

    feat: about personal development

---
+++ app/_components/MyProjectCard.tsx
@@ -0,0 +1,55 @@
+import Image from 'next/image';
+import NextLink from 'next/link';
+import OpenInNew from '@mui/icons-material/OpenInNew';
+
+type MyProjectCardProps = {
+  iconPath: string;
+  title: string;
+  link: string;
+  description: string;
+  tags: string[];
+};
+
+const MyProjectCard: React.FC<MyProjectCardProps> = ({
+  iconPath,
+  title,
+  link,
+  description,
+  tags
+}) => {
+  return (
+    <div className="mb-3 p-4 bg-white rounded-lg shadow-md">
+      <div className="flex items-center space-x-2">
+        <Image
+          src={iconPath}
+          alt="Avatar"
+          height={40}
+          width={40}
+          className="rounded-full object-cover w-10 h-10"
+        />
+        <NextLink
+          href={link}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-lg font-semibold"
+        >
+          {title}
+        </NextLink>
+        <OpenInNew fontSize="small" />
+      </div>
+      <p className="text-gray-600 mt-2">{description}</p>
+      <div className="flex flex-wrap mt-2">
+        {tags.map((tag, index) => (
+          <span
+            key={index}
+            className="bg-green-200 text-green-800 text-sm font-semibold m-1 px-2.5 py-0.5 rounded"
+          >
+            {tag}
+          </span>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default MyProjectCard;
--- app/page.tsx
+++ app/page.tsx
@@ -4,30 +4,50 @@ import VideoSummaries from './_components/VideoSummaries';
 import ChannelOnerLabel from './_components/ChannelOnerLabel';
 import OverviewColomn from './_components/OverviewColomn';
 import AssessmentIcons from './_components/AssessmentIcons';
+import MyProjectCard from './_components/MyProjectCard';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between pt-16">
-      <div className="flex min-h-screen flex-col">
-        <div className="w-hull text-sm ml-6">
-          <div className="p-4">
-            <div className="md:flex w-hull">
-              <div className="p-2 md:w-8/12">
-                <VideoPlayer videoUrl="https://www.youtube.com/embed/j8VN_RhkdCQ?si=znTbFe3uqHfyximi" />
-                <VideoSummaries />
-                <div className="md:flex items-center w-full mb-2">
-                  <div className="md:w-3/5">
-                    <ChannelOnerLabel />
-                  </div>
-                  <div className="flex space-x-4 my-2 md:w-2/5 md:justify-end">
-                    <AssessmentIcons />
-                  </div>
+    <main className="pt-16">
+      <div className="w-hull text-sm ml-3">
+        <div className="p-4">
+          <div className="md:flex w-hull">
+            <div className="p-2 md:w-8/12">
+              <VideoPlayer videoUrl="https://www.youtube.com/embed/j8VN_RhkdCQ?si=znTbFe3uqHfyximi" />
+              <VideoSummaries />
+              <div className="md:flex items-center w-full mb-2">
+                <div className="md:w-3/5">
+                  <ChannelOnerLabel />
                 </div>
-                <OverviewColomn />
+                <div className="flex space-x-4 my-2 md:w-2/5 md:justify-end">
+                  <AssessmentIcons />
+                </div>
               </div>
-              <div className="p-2 w-4/12">
-                <div className="mt-8"></div>
-              </div>
+              <OverviewColomn />
+            </div>
+            <div className="p-2 md:w-4/12">
+              <h2 className="py-4 text-lg font-bold">個人開発</h2>
+              <MyProjectCard
+                iconPath="/NetflixExtension.png"
+                title="Make More MyList in Netflix"
+                link="https://chromewebstore.google.com/detail/make-more-mylist-in-netfl/bicnagkkmhehedgogcopdadkakldblfk"
+                description="Netflixのマイリストをより快適にするためのGoogleChrome拡張機能を開発しました。より使いやすいアイデアが浮かべば都度アップデートしています。"
+                tags={['Vue.js', 'TypeScript', 'pinia']}
+              />
+              <MyProjectCard
+                iconPath="/omoidemeisho.png"
+                title="おもいでメイショ"
+                link=""
+                description="画像に点数、コメント、住所などを設定しアルバム管理できるモバイルアプリを個人開発しました。現在は諸般の事情により非公開となっています。"
+                tags={['ReactNative', 'TypeScript', 'Firebase', 'Android']}
+              />
+              <MyProjectCard
+                iconPath="/npmIcon.png"
+                title="Camo-tag"
+                link="https://www.npmjs.com/~camo-tag"
+                description="子要素はそのままに、特定のタグだけを自由にレンダリングできる特別なタグであるCamoを提供するnpmパッケージを開発しました。"
+                tags={['Vue', 'React', 'TypeScript', 'npm']}
+              />
             </div>
           </div>
         </div>
