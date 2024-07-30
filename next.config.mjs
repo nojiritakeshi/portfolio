c59c2122f9b67150ee745f19650900571673a6d5
nojiritakeshi <takesoccer08nine@ezweb.ne.jp>
2024-07-31 01:01:34 +0900

    fix/cd-node version

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   images: {
     domains: ['picsum.photos']
   },
-  output: 'export'
+  output: 'export',
+  basePath: isProd ? '/portfolio' : ''
 };
 
 export default nextConfig;
